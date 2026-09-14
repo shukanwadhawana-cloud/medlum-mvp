83dcc065ff6fa9286927a302e5ae018944745255
shukanwadhawana-cloud <shukanwadhawana@gmail.com>
2026-09-14 16:54:52 +0530

    feat: wire privacy consent into app shell

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,7 +1,13 @@
 "use client";
 
 import { DoctorProvider } from "./DoctorProvider";
+import CookieConsent from "./CookieConsent";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <DoctorProvider>{children}</DoctorProvider>;
+  return (
+    <DoctorProvider>
+      {children}
+      <CookieConsent />
+    </DoctorProvider>
+  );
 }
